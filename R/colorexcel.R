aca159af43e7093014ec9537e26e697359f5e5ad
liuguofang <liuguofang2004@126.com>
2018-04-09 15:05:03 +0800

    Update colorexcel.R

--- R/colorexcel.R
+++ R/colorexcel.R
@@ -2,26 +2,25 @@
 #'
 #' This function can be used to color outliers of excel data.
 #' @filename the file name of a excel data.
-#' @threhold the threhold value which is checked.
+#' @threshold the threshold value which is checked.
 #' @variable the continous variable name of data.
-#' @more  a switch which TRUE and FALSE denote over and less than the threhold.
+#' @more  a switch which TRUE and FALSE denote over and less than the threshold.
 #' @sheetTh the order of sheet in the excel file.
 #' @color makered colour.
 #' @examples 
 
 
-colorexcel <- function(filename, threhold, variable, more = TRUE, sheetTh = 1, color=2)
+colorexcel <- function(filename, threshold, variable, more = TRUE, sheetTh = 1, color=2)
 {
 	require(XLConnect)
 	wb <- loadWorkbook(filename, create = TRUE)
-	dat = readWorksheet(wb, sheet = getSheets(wb)[sheetTh])
-	heavyCar <- createCellStyle(wb, name = paste(sample(c(LETTERS, 
-									letters, 0:9), 3), collapse = ""))
+	dat <- readWorksheet(wb, sheet = getSheets(wb)[sheetTh])
+	heavyCar <- createCellStyle(wb, name = paste(sample(c(LETTERS,letters, 0:9), 3), collapse = ""))
 	setFillPattern(heavyCar, fill = XLC$FILL.SOLID_FOREGROUND)
 	setFillForegroundColor(heavyCar, color = color)
 	if (more) {
-		rowIndex = which(dat[[variable]] > threhold)
-	} else rowIndex = which(dat[[variable]] < threhold)
+		rowIndex = which(dat[[variable]] > threshold)
+	} else rowIndex = which(dat[[variable]] < threshold)
 	
 	rowIndex <- rowIndex + 1
 	colIndex <- which(names(dat) == variable)
