04c1e561cf6a4ac3e414d888ed10efe5306ad5f8
Guofang Liu <liuguofang2004@126.com>
2018-04-12 18:29:25 +0800

    Update temp.dayscale.plot.ibutton.R

--- R/temp.dayscale.plot.ibutton.R
+++ R/temp.dayscale.plot.ibutton.R
@@ -3,15 +3,8 @@
 #' This function can be used to plot dayscale temperature and humidity graph.
 #' @temp the name of temperature file.
 #' @humidity the name of humidity file. Default is drawing dayscale temperature curve. 
-#' @plot.filename the name of output plot. 
-#' @width the width of plot size. Default is 7 unit.
-#' @height the height of plot size. Default is 6 unit.
-#' @res the resolution of the plot. Default is 300 DPI.
 #' @start.time the start point of temperature or humidity graph
-temp.dayscale.plot.ibutton <- function(temp=temperature,humidity=NULL,plot.filename = plot.filename,
-		width =7,height = 6,res=300,start.time='07:00'){
-	 tiff(sprintf('%s(%s).tiff',plot.filename,Sys.Date()),width = width,
-			 height = height,units = 'in', compression = 'lzw', res=res)	 
+temp.dayscale.plot.ibutton <- function(temp=temperature,humidity=NULL,start.time='07:00'){	 
 	 if(!is.null(humidity))  layout(matrix(1:2,ncol=1))
 	 par(mar=rep(1,4),omi=c(0.5,0.7,0.1,0.1))
 	 plot(Value.mean~day,data=temperature,type='n',xaxt='n',ylab='Mean temperature per day (%)',xlab='')
@@ -30,4 +23,3 @@ temp.dayscale.plot.ibutton <- function(temp=temperature,humidity=NULL,plot.filen
 	 axis(1,at=time.pos,label=substr(time.pos,12,16),line=1) 
 	 dev.off()
  }
- 
