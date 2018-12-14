e2d3ac9e10802fb7e0ab225192e57520d20a5e49
Guofang Liu <liuguofang2004@126.com>
2018-12-15 07:32:16 +0800

    fix journal argument bug

--- R/sci.search.R
+++ R/sci.search.R
@@ -1,15 +1,17 @@
 ## internal functions 
 
-sci.search <- function(dat = sci,journal,type=NULL,IF5.threshold=NULL,yr=NULL){
+sci.search <- function(dat = sci,journal=NULL,type=NULL,IF5.threshold=NULL,yr=NULL){
 
     if(!is.null(type)) {
         dat <-  subset(dat,get(type)!='')
         type <- paste0(toupper(substr(type,1,1)),substr(type,2,nchar(type)))
         }
     dat <- dat[with(dat,order(year,decreasing=TRUE)),]
+    if(is.null(journal))  journal <- '.'
     pos <- unlist(lapply(1:length(journal),
-              function(x) grep(tolower(journal[x]),tolower(dat$Full.Journal.Title))))
+                         function(x) grep(tolower(journal[x]),tolower(dat$Full.Journal.Title))))
     case <- dat[pos,]
+                            
     if(!is.null(IF5.threshold)) {
         options(warn=-1)
         case <- case[as.numeric(as.character(case$IF5))>IF5.threshold,]
