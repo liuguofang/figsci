ddb1adccfcd8a79603ead88afe6c572a00be27f3
Guofang Liu <liuguofang2004@126.com>
2018-12-14 15:16:03 +0800

    Create internal.R

---
+++ R/internal.R
@@ -0,0 +1,19 @@
+sci.search <- function(dat = sci,journal,type=NULL,IF5.threshold=NULL,yr=NULL){
+    # type can be one of Ecology, Plant, Bio.Conservatation, Environment
+    
+    if(!is.null(type)) {
+        dat <-  subset(dat,get(type)!='')
+        type <- paste0(toupper(substr(type,1,1)),substr(type,2,nchar(type)))
+        }
+    dat <- dat[with(dat,order(year,decreasing=TRUE)),]
+    pos <- unlist(lapply(1:length(journal),
+              function(x) grep(tolower(journal[x]),tolower(dat$Full.Journal.Title))))
+    case <- dat[pos,]
+    if(!is.null(IF5.threshold)) 
+        options(warn=-1)
+        case <- case[as.numeric(as.character(case$IF5))>IF5.threshold,]
+        options(warn=0)
+    case <- subset(case,!is.na(Rank))
+    if(!is.null(yr))                         
+        case[with(case,year %in% yr),] else case                         
+}
