6e229d823133aa39558dd640b71effab96e4cd81
Guofang Liu <liuguofang2004@126.com>
2019-02-01 13:37:42 +0800

    Update twoway.barplot.R

--- R/twoway.barplot.R
+++ R/twoway.barplot.R
@@ -9,8 +9,8 @@
 #' @maximum  Which is used to modify visualtion on significant letters in figure. 
 #' @export
 #' @examples
-twoway.barplot <-function(dat, x.factor = "x.factor",response = "response", group = "group", method = 'HSD.test', maximum=NULL,minmum=NULL,legend=TRUE,
-                          legend.pos='topright',group.name=group, ...) {
+twoway.barplot <-function(dat, x.factor = "x.factor",response = "response", group = "group", method = 'HSD.test', maximum=NULL,
+                          minmum=NULL,legend=TRUE, sig.letters=NULL, legend.pos='topright',group.name=group, ...) {
   library(sciplot)
   library(doBy)
   library(agricolae)
@@ -26,26 +26,27 @@ twoway.barplot <-function(dat, x.factor = "x.factor",response = "response", grou
   group.range = cbind(split(CI, CI$group)[[1]]["h"], split(CI, CI$group)[[2]]["l"])
   para = bargraph.CI(x.factor, response, group, ylim = c(ifelse(is.null(minmum),0,minmum), 
                                                          ifelse(is.null(maximum),Max*1.2,maximum)), data = dat,...)
-  
-  group.level = levels(dat$group)
-  x.factor.level = levels(dat$x.factor)
-  split.dat = split(dat, dat$group)
-  lett.group <- lett.xfactor <-  matrix(NA, ncol = length(x.factor.level), nrow = length(group.level))
-  
-  for (i in 1:length(group.level)) {
-    model = aov(response ~ x.factor, data = split.dat[[i]])
-    lett = get(method)(model, "x.factor")$group
-    lett.group[i,] = toupper(lett[match(x.factor.level,row.names(lett)),'groups'])
-  }
-  
-  for (j in 1:length(x.factor.level)) {
-    oneset = subset(dat, x.factor == x.factor.level[j])
-    model2 = aov(response ~ group, data = oneset)
-    lett2 = get(method)(model2, "group")$group
-    lett.xfactor[,j] <- as.character(lett2[match(group.level,row.names(lett2)),'groups'])
-  }
-  
+  if(is.null(sig.letters)){
+      group.level = levels(dat$group)
+      x.factor.level = levels(dat$x.factor)
+      split.dat = split(dat, dat$group)
+      lett.group <- lett.xfactor <-  matrix(NA, ncol = length(x.factor.level), nrow = length(group.level))
+
+      for (i in 1:length(group.level)) {
+        model = aov(response ~ x.factor, data = split.dat[[i]])
+        lett = get(method)(model, "x.factor")$group
+        lett.group[i,] = toupper(lett[match(x.factor.level,row.names(lett)),'groups'])
+      }
+
+      for (j in 1:length(x.factor.level)) {
+        oneset = subset(dat, x.factor == x.factor.level[j])
+        model2 = aov(response ~ group, data = oneset)
+        lett2 = get(method)(model2, "group")$group
+        lett.xfactor[,j] <- as.character(lett2[match(group.level,row.names(lett2)),'groups'])
+      }
+
   sig.letters=matrix(paste0(matrix(lett.group),matrix(lett.xfactor)),ncol=length(x.factor.level))
+  }
   for (i in 1:length(x.factor.level)) 
     text(para$xvals[, i], para$CI[2, , i] + par("usr")[4] * 0.03, sig.letters[, i]) 
   if(legend)
