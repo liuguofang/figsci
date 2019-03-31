64fe57483b130ec9e54decf5f5a3b90ed0c9c2a0
Guofang Liu <liuguofang2004@126.com>
2019-03-31 09:24:52 +0800

    Update pvalue2display.R

--- R/pvalue2display.R
+++ R/pvalue2display.R
@@ -1,11 +1,20 @@
-pvalue2display <- function(p){
-   p <- round(p,3)
-   for(i in 1:length(p))
-     if(p[i]<0.001) 
-       p[i] <- '<0.001'
-     if(p[i]<0.05) 
-       p[i] <- '<0.05'
-    tag <- grep("<",p)
-    p[-tag] <- paste0("==",p[-tag])
-    p
+pvalue2display <- function(p,p.r2.on = FALSE){
+   if(!p.r.no){
+      p <- round(p,3)
+      for(i in 1:length(p))
+         if(p[i]<0.001) 
+            p[i] <- '<0.001'
+         tag <- grep("<",p)
+      p[-tag] <- paste0("==",p[-tag])
+      p
+   } esle {
+      r2 <- round(p, 2)
+      for(i in 1:length(r2))
+         if(r2[i]<0.01)
+            r2[i]<-'<0.01'
+         tag <- grep('<',r2)
+      r2[-tag] <- paste0("==",r2[-tag])
+      r2
+      
+   }
 }
