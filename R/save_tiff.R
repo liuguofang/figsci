12812068ff329bc571f0081d5d69d6a0ad24f091
Guofang Liu <liuguofang2004@126.com>
2019-10-10 15:45:15 +0800

    Create save_tiff.R

---
+++ R/save_tiff.R
@@ -0,0 +1,8 @@
+
+save_tiff <- function (fn, width = 6, height = 5, pointsize = 14, res=300)
+{
+	dev.copy(device = tiff, file = paste(fn, ".tiff",sep = ""), 
+		width = width, height = height,	pointsize = pointsize,
+		units = "in",res = res, compression = 'lzw')
+	dev.off()
+}
