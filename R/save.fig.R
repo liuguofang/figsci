ffda346eb0b9bae0d9d88a2878ea835e102672d8
Guofang Liu <liuguofang2004@126.com>
2019-10-11 11:32:23 +0800

    Update save.fig.R

--- R/save.fig.R
+++ R/save.fig.R
@@ -1,17 +1,17 @@
 
 save.fig <- function (fn, width = 6, height = 5, pointsize = 12, res=300)
 {
-	dev.copy(device = tiff, file = paste(fn, ".tiff",
-					sep = ""), width = width, height = height,
-			pointsize = pointsize,units = "in",res=res,compression = 'lzw')
+	dev.copy(device = tiff, file = paste(fn, ".tiff",sep = ""), 
+		 width = width, height = height,
+		 pointsize = pointsize,units = "in", res = res,
+		 compression = 'lzw')
 	dev.off()
-	dev.copy(device = postscript, file = paste(fn, ".eps",
-					sep = ""),
-			width = width, height = height,
-			pointsize = pointsize)
+	dev.copy(device = postscript, file = paste(fn, ".eps", sep = ""),
+		 width = width, height = height,
+		 pointsize = pointsize)
 	dev.off()
 	dev.copy(device = pdf, file = paste(fn, ".pdf", sep = ""),
-			width = width, height = height,
-			pointsize = pointsize)
+		 width = width, height = height,
+		 pointsize = pointsize)
 	dev.off()
 }
