8f1f8133012d7cabad43b0a20fe0320e8d5e8a64
Guofang Liu <liuguofang2004@126.com>
2026-03-30 14:42:29 +0800

    Update data2excel.R

--- R/data2excel.R
+++ R/data2excel.R
@@ -1,4 +1,5 @@
 data2excel <- function(dat, filename, sheetname, create =TRUE, ...) {
+	dat <- cbind(row.name = rownames(dat), dat)
 	library(XLConnect)
 	wb  <-  loadWorkbook(filename,  create  =  create)
 	createSheet(wb,  name  =  sheetname)
