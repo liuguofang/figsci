0974ec76004c36811fd492fbd3531c2b5d30cc1f
Guofang Liu <liuguofang2004@126.com>
2026-03-30 13:30:01 +0800

    Update author.packages.R

--- R/author.packages.R
+++ R/author.packages.R
@@ -1,9 +1,21 @@
-author.packages <- function(author,...){
-	ob <- sos::findFn(author,...)
-	packs <- sub("library/",'',unique(ob$Package))
-	packs.web <- sprintf("https://cran.r-project.org/web/packages/%s/index.html",packs)
-	out <- plyr::llply(packs.web,readLines)
-	author2 <- plyr::laply(out,.fun=function(lst){
-				if(length(grep(author,lst))) 'author' else 'no author'})
-	unique(packs[author2=='author'])
+author.packages <- function(author,ignore.case = TRUE,...){
+  if (!requireNamespace("sos", quietly = TRUE)) stop("Please install 'sos' package.")
+  if (!requireNamespace("plyr", quietly = TRUE)) stop("Please install 'plyr' package.")
+  
+  ob <- sos::findFn(author, ...)
+  packs <- sub("library/", '', unique(ob$Package))
+  packs.web <- sprintf("https://cran.r-project.org/web/packages/%s/index.html", packs)
+  
+  message("Checking ", length(packs), " packages on CRAN...")
+  out <- plyr::llply(packs.web, function(url) {
+    tryCatch(readLines(url, warn = FALSE), error = function(e) character(0))
+  }, .progress = "text")
+  
+  author2 <- plyr::laply(out, function(lst) {
+    if (length(grep(author, lst, ignore.case = ignore.case))) 'author' else 'no author'
+  })
+  
+  res <- unique(packs[author2 == 'author'])
+  print(res)
+  invisible(res)
 }
