cd0b06a7d71ec10b286adbf0d29ee3edb7648782
Guofang Liu <liuguofang2004@126.com>
2026-03-30 16:10:21 +0800

    Update vector_diff_compare.R

--- R/vector_diff_compare.R
+++ R/vector_diff_compare.R
@@ -1,7 +1,13 @@
 vector_diff_compare <- function(x, y) {
-	library(Hmisc)
-	result <- list(x.not.y = x[x %nin% y], y.not.x = y[y %nin% x])
-	names(result) <- c(sprintf("%s.not.%s", deparse(substitute(x)),  deparse(substitute(y))),
-			sprintf("%s.not.%s",  deparse(substitute(y)), deparse(substitute(x))))
-	result
+	if (!is.vector(x) || !is.vector(y)) stop("Both x and y must be vectors.")
+	    
+	    x_not_y <- x[!x %in% y]
+	    y_not_x <- y[!y %in% x]
+	    
+	    result <- list(x_not_y, y_not_x)
+	    names(result) <- c(
+	      sprintf("%s_not_%s", deparse(substitute(x)), deparse(substitute(y))),
+	      sprintf("%s_not_%s", deparse(substitute(y)), deparse(substitute(x)))
+	    )
+	    return(result)
 }
