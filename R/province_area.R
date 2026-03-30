20438b8ec3c1908ad9c5786a37989c7b553401f3
Guofang Liu <liuguofang2004@126.com>
2026-03-30 16:32:42 +0800

    Update and rename province.area.R to province_area.R

---
+++ R/province_area.R
@@ -0,0 +1,15 @@
+province_rea <- function(province){
+
+	library(sf)
+    nc <- st_read(shpfile, quiet = TRUE)
+	tag <- grep(province, nc$NAME)
+    if (length(tag) == 0) stop("Province not found.")
+	  
+	province_poly <- st_union(nc[tag, ])
+    province_poly_proj <- st_transform(province_poly, 3857)
+	 
+	area_m2 <- st_area(province_poly_proj)
+    area_km2 <- as.numeric(area_m2) / 1e6
+  
+    return(area_km2)
+}
