2f767c32ec380218747d5341e7ebcebe7f76d9cd
Guofang Liu <liuguofang2004@126.com>
2026-03-31 20:21:13 +0800

    Update and rename save_fig.R to save_figure.R

--- R/save_figure.R
+++ R/save_figure.R
@@ -1,5 +1,5 @@
 
-save_fig <- function (fn, width = 6, height = 5, pointsize = 12, res=300)
+save_figure <- function (fn, width = 6, height = 5, pointsize = 12, res=300)
 {
 	dev.copy(device = tiff, file = paste(fn, ".tiff",sep = ""), 
 		 width = width, height = height,
