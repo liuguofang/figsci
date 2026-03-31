8b845eaafa9db082d64bdc73b1dea57dc1696f7a
Guofang Liu <liuguofang2004@126.com>
2026-03-31 10:56:45 +0800

    Update and rename color.alpha.R to color_alpha.R

--- R/color_alpha.R
+++ R/color_alpha.R
@@ -4,7 +4,7 @@
 #' @color Feed a color
 #' @export
 #' @examples
-color.alpha <- function(color = "red", ...) {
+color_alpha <- function(color = "red", ...) {
 	
 	vector <- as.vector(col2rgb(color))/255
 	rgb(vector[1], vector[2], vector[3], ...)
