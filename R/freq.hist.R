0f8a732b3ed9bda40cabc621854aa65f1bf64616
Guofang Liu <liuguofang2004@126.com>
2026-03-31 13:47:13 +0800

    Update freq_hist.R

--- R/freq.hist.R
+++ R/freq.hist.R
@@ -1,10 +1,27 @@
-freq.hist <- function(dat = NULL, ...) {
-    if (missing(dat)) {
-        set.seed(1)
+freq_hist <- function(dat = NULL, bins = 30) {
+    library(ggplot2)
+    if (is.null(dat)) {
+        set.seed(123)
         dat <- rnorm(1000, 50, 10)
     }
-    H <- hist(dat, freq = TRUE, ...)  ## This will plot the histogram as well
-    dx <- min(diff(H$breaks))
-    curve(length(dat) * dx * dnorm(x, mean(dat), sd(dat)), add = TRUE, 
-        col = "blue")
-} 
+    n <- length(dat)
+    m <- mean(dat, na.rm = TRUE)
+    s <- sd(dat, na.rm = TRUE)
+    cv <- s / m
+    df <- data.frame(x = dat)
+    hist_data <- hist(dat, breaks = bins, plot = FALSE)
+    max_y <- max(hist_data$counts)
+    x_pos <- max(dat) - 0.15 * diff(range(dat))
+    y_pos <- max_y - 0.05 * max_y
+    p <- ggplot(df, aes(x = x)) +
+        geom_histogram(aes(y = ..count..), bins = bins, fill = "grey80", color = "black") +
+        stat_function(fun = function(x) n * diff(range(dat))/bins * dnorm(x, m, s), 
+                      color = "blue", size = 1) +
+        ggthemes::theme_few() +
+        annotate("text", 
+                 x = x_pos, y = y_pos, 
+                 label = sprintf("n = %d\nmean = %.2f\nsd = %.2f\nCV = %.2f", n, m, s, cv),
+                 hjust = 0, vjust = 1, size = 4)
+    print(p)
+}
+
