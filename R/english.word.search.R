623949a6b9f7a2b41cbc61b24daf4d3c1436fc56
Guofang Liu <liuguofang2004@126.com>
2026-03-31 13:26:55 +0800

    Delete R/english.word.search.R

--- R/english.word.search.R
+++
@@ -1,9 +0,0 @@
-english.word.search <- function(string=''){
-	tag <- grep(string,english.words$words)
-	if(length(tag)==0)
-		tag <- grep(string,english.words$meaning)
-	out <- english.words[tag,]
-
-	fix(out) # show the results
-}
-
