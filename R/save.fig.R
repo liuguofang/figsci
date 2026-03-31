5e7b2a3daffd8fa1c356e13a4691e8179a3c5df1
Guofang Liu <liuguofang2004@126.com>
2026-03-31 15:10:35 +0800

    Update save.fig.R

--- R/save.fig.R
+++ R/save.fig.R
@@ -1,5 +1,5 @@
 
-save.fig <- function (fn, width = 6, height = 5, pointsize = 12, res=300)
+save_fig <- function (fn, width = 6, height = 5, pointsize = 12, res=300)
 {
 	dev.copy(device = tiff, file = paste(fn, ".tiff",sep = ""), 
 		 width = width, height = height,
