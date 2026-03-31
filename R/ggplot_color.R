3146db7c1519a5a60ca10d4c45a6bcfbe2a7fd93
Guofang Liu <liuguofang2004@126.com>
2026-03-31 14:39:57 +0800

    Update and rename ggplot.color.R to ggplot_color.R

--- R/ggplot_color.R
+++ R/ggplot_color.R
@@ -1,4 +1,4 @@
-ggplot.color <- function(n){
+ggplot_color <- function(n){
 	library(ggplot2)
 	sample.dat <- function(n) data.frame(x=rnorm(10*n),y=rnorm(n*10),group=factor(rep(1:n,each=10)))
 	p <- ggplot(sample.dat(n),aes(x=x,y=y,color=group))+geom_point(aes(colour=group))
